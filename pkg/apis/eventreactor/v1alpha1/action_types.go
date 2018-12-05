787d1fdf16f49c357fd9545dfa3b79df758b1f32
Moto Ishizawa <summerwind.jp@gmail.com>
2018-12-05 23:20:08 +0900

    Add Action resource

---
+++ pkg/apis/eventreactor/v1alpha1/action_types.go
@@ -0,0 +1,60 @@
+/*
+Copyright 2018 The Event Reactor Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	buildv1alpha1 "github.com/knative/build/pkg/apis/build/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// ActionSpec defines the desired state of Action
+type ActionSpec struct {
+	buildv1alpha1.BuildSpec
+}
+
+// ActionStatus defines the observed state of Action
+type ActionStatus struct {
+	buildv1alpha1.BuildStatus
+
+	StepLogs []string `json:"stepLogs,omitempty"`
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Action is the Schema for the actions API
+// +k8s:openapi-gen=true
+type Action struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ActionSpec   `json:"spec,omitempty"`
+	Status ActionStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ActionList contains a list of Action
+type ActionList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Action `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Action{}, &ActionList{})
+}
