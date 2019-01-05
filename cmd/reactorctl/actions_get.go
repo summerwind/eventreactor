58eef298f93c6f6fd18da0b04cac41e0e48bcb80
Moto Ishizawa <summerwind.jp@gmail.com>
2019-01-05 12:50:25 +0900

    Improve output of action

--- cmd/reactorctl/actions_get.go
+++ cmd/reactorctl/actions_get.go
@@ -4,12 +4,31 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"k8s.io/apimachinery/pkg/types"
 
 	"github.com/summerwind/eventreactor/pkg/apis/eventreactor/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+type Action struct {
+	Name     string
+	Date     metav1.Time
+	Event    string
+	Pipeline string
+	Steps    []ActionStep
+}
+
+type ActionStep struct {
+	Name       string
+	Reason     string
+	ExitCode   int32
+	StartedAt  metav1.Time
+	FinishedAt metav1.Time
+	Log        string
+}
 
 func NewActionsGetCommand() *cobra.Command {
 	var cmd = &cobra.Command{
@@ -38,7 +57,37 @@ func actionsGetRun(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	out, err := render(actionTemplate, action)
+	a := &Action{
+		Name:     action.Name,
+		Date:     action.ObjectMeta.CreationTimestamp,
+		Event:    action.Spec.Event,
+		Pipeline: action.Spec.Pipeline.Name,
+		Steps:    []ActionStep{},
+	}
+
+	for i, s := range action.Status.BuildStatus.StepsCompleted {
+		if i == 0 {
+			continue
+		}
+
+		state := action.Status.StepStates[i].Terminated
+		if state == nil {
+			continue
+		}
+
+		as := ActionStep{
+			Name:       strings.Replace(s, "build-step-", "", -1),
+			Reason:     state.Reason,
+			ExitCode:   state.ExitCode,
+			StartedAt:  state.StartedAt,
+			FinishedAt: state.FinishedAt,
+			Log:        action.Status.StepLogs[i],
+		}
+
+		a.Steps = append(a.Steps, as)
+	}
+
+	out, err := render(actionTemplate, a)
 	if err != nil {
 		return err
 	}
@@ -49,11 +98,17 @@ func actionsGetRun(cmd *cobra.Command, args []string) error {
 }
 
 const actionTemplate = `
-Name: {{ .ObjectMeta.Name }}
-Date: {{ .ObjectMeta.CreationTimestamp }}
+Name:     {{ .Name }}
+Event:    {{ .Event }}
+Pipeline: {{ .Pipeline }}
+Date:     {{ .Date }}
 
-{{ range $i, $s := .Status.BuildStatus.StepsCompleted -}}
-[{{ $s }}]
-{{ index $.Status.StepLogs $i }}
+{{ range $i, $s := .Steps -}}
+[ {{ $s.Name }} ]
+Started At: {{ $s.StartedAt }}
+Finished At: {{ $s.FinishedAt }}
+Exit Code: {{ $s.ExitCode }}
+-------------------
+{{ $s.Log }}
 {{ end -}}
 `
