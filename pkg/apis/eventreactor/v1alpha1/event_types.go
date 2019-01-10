b2d4f95508fa60cee4b1dd2c0eff6187a54d5d7f
Moto Ishizawa <summerwind.jp@gmail.com>
2019-01-10 14:15:34 +0900

    Remove subresource definition

--- pkg/apis/eventreactor/v1alpha1/event_types.go
+++ pkg/apis/eventreactor/v1alpha1/event_types.go
@@ -53,7 +53,6 @@ type EventStatus struct {
 
 // Event is the Schema for the events API
 // +k8s:openapi-gen=true
-// +kubebuilder:subresource:status
 type Event struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
