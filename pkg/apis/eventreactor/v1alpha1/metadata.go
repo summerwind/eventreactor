7ece63d40874fb4ec9fd9a061e10bdcc1d3e5402
Moto Ishizawa <summerwind.jp@gmail.com>
2019-01-10 22:28:36 +0900

    Add metadata key for notification

--- pkg/apis/eventreactor/v1alpha1/metadata.go
+++ pkg/apis/eventreactor/v1alpha1/metadata.go
@@ -1,7 +1,11 @@
 package v1alpha1
 
 const (
-	KeyEventName    = "event.eventreactor.summerwind.github.io/name"
-	KeyEventType    = "event.eventreactor.summerwind.github.io/type"
-	KeyPipelineName = "pipeline.eventreactor.summerwind.github.io/name"
+	KeyEventName = "event.eventreactor.summerwind.github.io/name"
+	KeyEventType = "event.eventreactor.summerwind.github.io/type"
+
+	KeyPipelineName           = "pipeline.eventreactor.summerwind.github.io/name"
+	KeyPipelineUpstreamName   = "pipeline.eventreactor.summerwind.github.io/upstream-name"
+	KeyPipelineUpstreamStatus = "pipeline.eventreactor.summerwind.github.io/upstream-status"
+	KeyPipelineVia            = "pipeline.eventreactor.summerwind.github.io/via"
 )
