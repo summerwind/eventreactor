02b053c700d38c60c13d514b67e7255f9e4a0344
Moto Ishizawa <summerwind.jp@gmail.com>
2019-01-12 00:12:16 +0900

    Remove unused keys

--- pkg/apis/eventreactor/v1alpha1/metadata.go
+++ pkg/apis/eventreactor/v1alpha1/metadata.go
@@ -1,11 +1,7 @@
 package v1alpha1
 
 const (
-	KeyEventName = "event.eventreactor.summerwind.github.io/name"
-	KeyEventType = "event.eventreactor.summerwind.github.io/type"
-
-	KeyPipelineName           = "pipeline.eventreactor.summerwind.github.io/name"
-	KeyPipelineUpstreamName   = "pipeline.eventreactor.summerwind.github.io/upstream-name"
-	KeyPipelineUpstreamStatus = "pipeline.eventreactor.summerwind.github.io/upstream-status"
-	KeyPipelineVia            = "pipeline.eventreactor.summerwind.github.io/via"
+	KeyEventName    = "event.eventreactor.summerwind.github.io/name"
+	KeyEventType    = "event.eventreactor.summerwind.github.io/type"
+	KeyPipelineName = "pipeline.eventreactor.summerwind.github.io/name"
 )
