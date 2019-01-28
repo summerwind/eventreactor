96fa4f4a77af1c51912c1943931a80339f85be9b
Moto Ishizawa <summerwind.jp@gmail.com>
2019-01-28 23:24:11 +0900

    Add more tests for event controller

--- pkg/controller/event/event_controller_test.go
+++ pkg/controller/event/event_controller_test.go
@@ -21,14 +21,17 @@ import (
 	"time"
 
 	"github.com/onsi/gomega"
-	v1alpha1 "github.com/summerwind/eventreactor/pkg/apis/eventreactor/v1alpha1"
 	"golang.org/x/net/context"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	buildv1alpha1 "github.com/knative/build/pkg/apis/build/v1alpha1"
+	v1alpha1 "github.com/summerwind/eventreactor/pkg/apis/eventreactor/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var c client.Client
@@ -44,42 +47,24 @@ func TestReconcile(t *testing.T) {
 			Namespace: "default",
 		},
 		Spec: v1alpha1.EventSpec{
-			Type:   "io.github.summerwind.eventreactor.test",
-			Source: "/eventreactor/test",
+			Type:   "eventreactor.test",
+			Source: "/eventreactor/test/reconcile",
 			ID:     "f378179e-7d49-4078-84ce-e529de6dfdca",
 			Time:   &now,
 		},
 	}
 
-	pipeline := &v1alpha1.Pipeline{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "pipeline",
-			Namespace: "default",
-			Labels: map[string]string{
-				v1alpha1.KeyEventType: "io.github.summerwind.eventreactor.test",
-			},
-		},
-		Spec: v1alpha1.PipelineSpec{
-			Trigger: v1alpha1.PipelineTrigger{
-				Event: &v1alpha1.PipelineTriggerEvent{
-					Type:          "io.github.summerwind.eventreactor.test",
-					SourcePattern: "/eventreactor/.*",
-				},
-			},
-		},
-	}
-
 	expected := reconcile.Request{
 		NamespacedName: types.NamespacedName{
-			Name:      "test",
-			Namespace: "default",
+			Name:      instance.Name,
+			Namespace: instance.Namespace,
 		},
 	}
 
 	g := gomega.NewGomegaWithT(t)
 
-	// Setup the Manager and Controller.  Wrap the Controller Reconcile function so it writes each request to a
-	// channel when it is finished.
+	// Setup the Manager and Controller. Wrap the Controller Reconcile function
+	// so it writes each request to a channel when it is finished.
 	mgr, err := manager.New(cfg, manager.Options{})
 	g.Expect(err).NotTo(gomega.HaveOccurred())
 	c = mgr.GetClient()
@@ -94,38 +79,91 @@ func TestReconcile(t *testing.T) {
 		mgrStopped.Wait()
 	}()
 
-	err = c.Create(context.TODO(), pipeline)
-	if apierrors.IsInvalid(err) {
-		t.Logf("failed to create pipeline, got an invalid object error: %v", err)
-		return
+	// Create pipelines
+	pipelines := [][]string{
+		[]string{"test-valid", "eventreactor.test", "/eventreactor/test/.*"},
+		[]string{"test-type-mismatched", "eventreactor.dummy", "/eventreactor/test/.*"},
+		[]string{"test-source-mismatched", "eventreactor.test", "/eventreactor/dummy/.*"},
 	}
-	g.Expect(err).NotTo(gomega.HaveOccurred())
-	defer c.Delete(context.TODO(), pipeline)
 
+	for _, info := range pipelines {
+		pipeline := &v1alpha1.Pipeline{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      info[0],
+				Namespace: "default",
+				Labels: map[string]string{
+					v1alpha1.KeyEventType: "eventreactor.test",
+					"test":                "yes",
+				},
+			},
+			Spec: v1alpha1.PipelineSpec{
+				BuildSpec: buildv1alpha1.BuildSpec{
+					Steps: []corev1.Container{
+						corev1.Container{
+							Name:  "hello",
+							Image: "ubuntu:18.04",
+							Args:  []string{"echo", "hello world"},
+						},
+					},
+				},
+				Trigger: v1alpha1.PipelineTrigger{
+					Event: &v1alpha1.PipelineTriggerEvent{
+						Type:          info[1],
+						SourcePattern: info[2],
+					},
+				},
+			},
+		}
+
+		err = c.Create(context.TODO(), pipeline)
+		if errors.IsInvalid(err) {
+			t.Logf("failed to create pipeline, got an invalid object error: %v", err)
+			return
+		}
+
+		g.Expect(err).NotTo(gomega.HaveOccurred())
+		defer c.Delete(context.TODO(), pipeline)
+	}
+
+	// Craete event
 	err = c.Create(context.TODO(), instance)
-	if apierrors.IsInvalid(err) {
+	if errors.IsInvalid(err) {
 		t.Logf("failed to create object, got an invalid object error: %v", err)
 		return
 	}
+
 	g.Expect(err).NotTo(gomega.HaveOccurred())
 	defer c.Delete(context.TODO(), instance)
 
+	// Wait for reconcile request
 	g.Eventually(requests, timeout).Should(gomega.Receive(gomega.Equal(expected)))
 
+	// Get actions
 	actionList := &v1alpha1.ActionList{}
-
 	labels := map[string]string{
-		v1alpha1.KeyEventName:    instance.Name,
-		v1alpha1.KeyPipelineName: pipeline.Name,
+		v1alpha1.KeyEventName: instance.Name,
 	}
-
 	opts := &client.ListOptions{Namespace: instance.Namespace}
 	opts = opts.MatchingLabels(labels)
 
 	g.Expect(c.List(context.TODO(), opts, actionList)).To(gomega.Succeed())
 	g.Expect(len(actionList.Items)).To(gomega.Equal(1))
 
-	// Manually delete Deployment since GC isn't enabled in the test control plane
+	// Test the value of action
 	action := actionList.Items[0]
-	g.Expect(c.Delete(context.TODO(), &action)).To(gomega.Succeed())
+	g.Expect(action.ObjectMeta.Labels[v1alpha1.KeyEventName]).To(gomega.Equal(instance.Name))
+	g.Expect(action.ObjectMeta.Labels[v1alpha1.KeyPipelineName]).To(gomega.Equal(pipelines[0][0]))
+	g.Expect(action.ObjectMeta.Labels[v1alpha1.KeyTransactionID]).NotTo(gomega.Equal(""))
+	g.Expect(action.ObjectMeta.Labels["test"]).To(gomega.Equal("yes"))
+	g.Expect(action.Spec.Event.Name).To(gomega.Equal(instance.Name))
+	g.Expect(action.Spec.Event.Type).To(gomega.Equal(instance.Spec.Type))
+	g.Expect(action.Spec.Event.Source).To(gomega.Equal(instance.Spec.Source))
+	g.Expect(action.Spec.Pipeline.Name).To(gomega.Equal(pipelines[0][0]))
+	g.Expect(action.Spec.Transaction.ID).NotTo(gomega.Equal(""))
+	g.Expect(action.Spec.Transaction.Stage).To(gomega.Equal(1))
+
+	// Manually delete action since GC isn't enabled in the test control plane
+	for _, action := range actionList.Items {
+		g.Expect(c.Delete(context.TODO(), &action)).To(gomega.Succeed())
+	}
 }
